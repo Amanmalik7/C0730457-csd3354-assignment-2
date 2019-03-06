9cd6c92149efde544d53d78ee4e134eb39ba77b0
visualstudio <visualstudio@122-PC05>
2019-03-06 11:26:53 -0500

    lab09

--- C0730457-csd3354-assignment-2/Program.cs
+++ C0730457-csd3354-assignment-2/Program.cs
@@ -10,33 +10,14 @@ namespace C0730457_csd3354_assignment_2
 
     namespace DelegatesAndEvents
     {
-        public class Program
-        {
-            public static void Main()
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-
-        }
         public delegate void MyDelegate();
-
-        public class DelegateExercises
+        public class DelegateExercises : MyDelegate
         {
-            void Method1()
-            {
-                System.Console.WriteLine("Method1");
-            }
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
         }
-
     }
 }
+
+
 
 
 
