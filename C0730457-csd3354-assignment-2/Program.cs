0f3248d4b08af368c512d7fef04f7c3ab006b347
visualstudio <visualstudio@122-PC05>
2019-03-06 10:36:30 -0500

    commit a

--- C0730457-csd3354-assignment-2/Program.cs
+++ C0730457-csd3354-assignment-2/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace C0730457_csd3354_assignment_2
 {
-    class Program
+    public class DelegateExercises
     {
-        static void Main(string[] args)
+        public delegate void MyDelegate();
+        void Method1()
         {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+    class program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
